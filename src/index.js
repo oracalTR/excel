9928be856bf5d4d8ca2b403a5649b2ce021b0e80
Pavel Trushnikov <oracal@mail.ru>
2022-01-30 18:18:33 +0300

    Конфигурация webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import "./scss/style.scss";
+import './scss/style.scss';
 
 console.log('Привет от WebPAck и DevServer');
+console.log();
+console.log();
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,37 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = (env, argv) => {
     const isProd = argv.mode === 'production';
     const isDev = !isProd;
-    const filename = (path, ext) => isProd ? `${path}/main.[contenthash]${ext}` : `${path}/main.${ext}`;
+    const filename = (path, ext) => isProd ?
+    `${path}/main.[contenthash]${ext}` : `${path}/main.${ext}`;
+    const plugins = () => {
+      const base = [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                  from: path.resolve(__dirname, 'src', 'images'),
+                  to: path.resolve(__dirname, 'dist', 'images')
+                },
+            ],
+          }),
+          new MiniCssExtractPlugin({
+            filename: filename('css', '.css'),
+          }),
+        ];
+        if (isDev) {
+          base.push(new ESLintPlugin());
+        }
+        return base;
+    };
+
     return {
         context: path.resolve(__dirname, 'src'),
         entry: {
@@ -28,21 +53,7 @@ module.exports = (env, argv) => {
                 '@core': path.resolve(__dirname, 'src', 'core'),
             }
         },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: './index.html'
-            }),
-            new CopyPlugin({
-                patterns: [
-                  { 
-                      from: path.resolve(__dirname, 'src', 'images'), 
-                      to: path.resolve(__dirname, 'dist', 'images') },
-                ],
-              }),
-              new MiniCssExtractPlugin({
-                filename: filename('css', '.css'),
-              }),
-        ],
+        plugins: plugins(),
         devtool: isDev ? 'source-map' : false,
         module: {
             rules: [
@@ -52,10 +63,20 @@ module.exports = (env, argv) => {
                   // Creates `style` nodes from JS strings
                   MiniCssExtractPlugin.loader,
                   // Translates CSS into CommonJS
-                  "css-loader",
+                  'css-loader',
                   // Compiles Sass to CSS
-                  "sass-loader",
+                  'sass-loader',
                 ],
+              },
+              {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
               },
             ],
           },
